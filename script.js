eceb8079ec4c93a4d3b75f3953f53054fefcb822
Ruan Cortez <ruanvictorcortez@gmail.com>
2020-09-23 15:41:37 -0300

    jogo-finalizado

---
+++ script.js
@@ -0,0 +1,64 @@
+const cards = document.querySelectorAll('.card');
+let hasFlippedCard = false;
+let firstCard, secondCard;
+let lockBoard = false;
+
+function flipCard() {
+    if(lockBoard) return;
+    if(this === firstCard) return;
+    
+    this.classList.add('flip');
+    if(!hasFlippedCard){
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+    hasFlippedCard = false;
+    checkForMath();
+}
+
+function checkForMath() {
+    if(firstCard.dataset.card === secondCard.dataset.card){
+        disableCards();
+        return;
+    }
+
+    unflipCards();
+}
+
+function disableCards() { 
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click',flipCard);
+
+    resetBoard();
+}
+
+function unflipCards() {
+    lockBoard = true;
+
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        resetBoard();
+    }, 1500);
+}
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null. null];
+}
+
+/* embaralhar as cartas*/
+(function shuffle() {
+    cards.forEach((card) => {
+        let randonPosition = Math.floor(Math.random() * 12);
+        card.getElementsByClassName.order = randonPosition;
+    })
+}());
+
+cards.forEach((card) =>{
+    card.addEventListener('click', flipCard)
+});
